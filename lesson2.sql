00eb4ee39cb49c8225c2fa3eae6f459565699867
Vitaliy Demchyshyn <demchyshyn.v87@gmail.com>
2023-10-04 20:28:21 +0300

    lesson1

--- lesson2.sql
+++ lesson2.sql
@@ -1,79 +1,77 @@
-# select distinct name from users;
-
+use userrrr;
 create table cities
 (
-    id   int auto_increment primary key,
+    id   int primary key auto_increment,
     city varchar(20) not null
 );
 
 create table users
 (
-    id      int auto_increment primary key,
+    id      int primary key auto_increment,
     name    varchar(20) not null,
     age     int         not null,
     city_id int         null,
     foreign key (city_id) references cities (id)
 );
 
+insert into cities
+values (null, 'Rivne');
 
-select *
-from cities;
+insert into users
+values (null, 'Petro', 18, null);
+
+
 select *
 from users;
-
-select users.*, city
-from users
-         right join cities c on c.id = users.city_id;
+select *
+from cities;
 
 select *
 from users
-         join cities c on c.id = users.city_id;
+         join cities on cities.id = users.city_id;
+select users.id, name, age, city
+from users
+         join cities on cities.id = users.city_id;
+select users.*, city
+from users
+         join cities on cities.id = users.city_id;
 
 select *
 from users
          left join cities c on c.id = users.city_id;
-
 select *
 from users
          right join cities c on c.id = users.city_id;
 
-
-update users join cities c on c.id = users.city_id
-set city_id=1
-where city = 'kyiv';
-
-delete users
+select users.*
 from users
          join cities c on c.id = users.city_id
-where city = 'rivne';
-delete
-from users
-where city_id = 2;
-
-delete
-from cities
 where city = 'lviv';
 
 
-select *
-from application
-         join client c on c.idClient = application.Client_idClient
-         join department d on d.idDepartment = c.Department_idDepartment;
-
-
-# select city
-# from cities
-#          join users u on cities.id = u.city_id
-# where age = (select min(age)
-#              from users
-#                       join cities c on c.id = users.city_id
-#              where city = 'kyiv');
-
-select *
+select city
 from users
-         right join cities c on c.id = users.city_id;
+         join cities c on c.id = users.city_id
+where users.id = 3;
 
-select * from users where age in (select age
-                                  from users
-                                           join cities c on c.id = users.city_id
-                                  where city='kyiv')
+
+select *
+from client
+         join userrrr.application a on client.idClient = a.Client_idClient
+         join userrrr.department d on d.idDepartment = client.Department_idDepartment;
+
+
+select distinct name from users;
+
+(select * from users order by age desc limit 1) union (select * from users order by age limit 1);
+
+
+
+
+select * from cities where id in (select city_id from users where name in ('karina','max', 'tolik'));
+select * from cities where id in (1,2);
+
+
+select count(*) as count, model from cars group by model order by count desc limit 1;
+
+
