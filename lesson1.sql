61abdb6ea910679759e369413c714823d9d4ca64
Vitaliy Dechyshyn <demchyshyn.v87@gmail.com>
2024-04-08 19:37:42 +0200

    jan-2024

--- lesson1.sql
+++ lesson1.sql
@@ -1,72 +1,70 @@
-use ajaj;
+use asdasd;
 
-show tables ;
+show databases;
 
-select * from users;
+select * from cars;
+select id, model, year from cars;
 
-drop table users;
-
-
-# create table users(
-                        #                       id int auto_increment primary key,
-                        #                       name varchar(20) not null ,
-                        #                       age int not null
-                            #
-                            # );
-
-insert into users values (22, 'Max', 18);
-
-select * from users;
-select id, name, age, gender  from users;
-
-
-select * from users where name='oLeG';
-select * from users where age=30 and gender='male';
-select * from users where age=30 or gender='male';
-
-select * from users where name like 'o%';
-select * from users where name like '%o%';
-select * from users where name like '__e%';
+select * from users where name = 'OLEG';
+select * from users where name != 'OLEG';
+select * from users where name like 'o%' ;
+select * from users where name like '%o%' ;
+select * from users where name like '__e%' ;
 
 select * from users where age = 30;
 select * from users where age != 30;
-select * from users where age between 20 and  30;
-select * from users where age not between 20 and  30;
-select * from users where age > 30;
-select * from users where age >= 30;
-select * from users where age < 30;
-select * from users where age <= 30;
+select * from users where age between 20 and 30;
+select * from users where age not between 20 and 30;
 
-select * from users where age in (20,45,16,25,19);
+select * from users where age >=20 and age<=30;
+select * from users where age >=20 or age<=30;
 
-select * from users where length(name) > 4;
+select * from users where age=16 and name like '_a%';
 
-select * from users where age > 20 order by age desc ;
-select * from users where age > 20 order by age desc limit 3;
-select * from users where age > 20 order by age desc limit 3 offset 6;
+select * from users where age in (20,25,30,45,14);
+select * from users where age not in (20,25,30,45,14);
+
+select * from users where length(name) = 4;
+
+select * from users order by name asc;
+select * from users order by name;
+select * from users order by name desc ;
+select * from users order by age desc ;
+
+select * from users order by age desc limit 2 offset 6;
 
 
-# min()
+#
 # max()
-# avg()
-# count()
-# sum()
+# min()
+#    avg()
+#       count()
+#            sum()
 
 select min(age) as minAge from users;
 select max(age) as maxAge from users;
-select sum(age) as sumAge from users;
-select avg(age) as avgAge from users;
+select sum(age) as sumOfAge from users;
+select avg(age) as avgOfAge from users;
+select count(*) as count from users;
 
+select count(*), gender from users where age<=25 group by gender;
+
+
+select count(*) as count, model from cars where year>2020 group by model having count>=2;
+
+# {'min(age)':4}
+
+
+create table users
+(
+    id int primary key auto_increment,
+    name varchar(20) not null ,
+    age int not null
+);
+
+insert into users values (null, 'Max', 15);
 select * from users;
 
-select min(age) as minAge, avg(age) as avgAge, gender from users group by gender;
-select count(*) as count, gender from users group by gender;
+delete  from users where id=6;
 
-select count(*) as modelCount, model  from cars where year>2020 group by model having modelCount>=2 order by modelCount desc ;
-
-select * from users;
-
-delete from users where id=9;
-
-update users set gender='male' where name='lina'
-
+update users set name='Karina' where id=5;
